994ee5b866208b190feec1f7aa522af781e002fb
Logan Faschingbauer <faschingbauerl@yahoo.com>
2023-05-26 02:27:50 -0500

    update theme based on style guide

--- src/styles/themes.tsx
+++ src/styles/themes.tsx
@@ -28,13 +28,20 @@ const defaultTheme = {
     laptopLg: "1440px",
     desktop: "2560px",
   },
-  color: {},
+  color: {
+    checkbox: {
+      background:
+        "linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+    },
+  },
   duration: {
     short: "250ms",
     medium: "375ms",
     long: "500ms",
   },
-  fontFamily: {},
+  fontFamily: {
+    primary: "Josefin Sans, sans-serif",
+  },
   fontSize: {
     base100: "0.75rem",
     base200: "0.875rem",
@@ -48,15 +55,8 @@ const defaultTheme = {
     base1000: "6rem",
   },
   fontWeight: {
-    thin: "100",
-    extraLight: "200",
-    light: "300",
     normal: "400",
-    medium: "500",
-    semiBold: "600",
     bold: "700",
-    extraBold: "800",
-    black: "900",
   },
   spacing: {
     base100: "0.125rem",
@@ -76,6 +76,18 @@ const darkTheme = {
   name: "dark",
   color: {
     ...defaultTheme.color,
+    gray: "hsl(234, 11%, 52%)",
+    background: {
+      app: "hsl(235, 21%, 11%)",
+      primary: "hsl(235, 24%, 19%)",
+    },
+    text: {
+      header: "hsl(0, 0%, 98%)",
+      primary: "hsl(234, 39%, 85%)",
+      secondary: "hsl(233, 14%, 35%)",
+      accent: "hsl(220, 98%, 61%)",
+      hover: "hsl(236, 33%, 92%)",
+    },
   },
 };
 
@@ -83,6 +95,18 @@ const lightTheme = {
   name: "light",
   color: {
     ...defaultTheme.color,
+    gray: "hsl(234, 11%, 52%)",
+    background: {
+      app: "hsl(236, 33%, 92%)",
+      primary: "hsl(0, 0%, 98%)",
+    },
+    text: {
+      header: "hsl(0, 0%, 98%)",
+      primary: "hsl(235, 19%, 35%)",
+      secondary: "hsl(236, 9%, 61%)",
+      accent: "hsl(220, 98%, 61%)",
+      hover: "hsl(235, 19%, 35%)",
+    },
   },
 };
 
